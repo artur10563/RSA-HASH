748cae081dbc2262575f33789b0ce8c466b547b1
artur10563 <galkaartur2004galkaartur@gmail.com>
2023-05-26 19:07:13 +0300

    Encryption

--- RSAMain/RSAMain/Program.cs
+++ RSAMain/RSAMain/Program.cs
@@ -8,32 +8,127 @@ using System.Text;
 using System.Threading.Tasks;
 using RSALIB;
 
+//DES(M || RSA_A(H(M))) || RSA_B(KS)
 namespace RSAMain
 {
     internal class Program
     {
-        
 
-        private static void ShowDict<K, V>(Dictionary<K,V> d) 
+
+        private static void ShowDict<K, V>(Dictionary<K, V> d)
         {
             foreach (var item in d)
             {
                 Console.WriteLine($"{item.Key} : {item.Value}");
             }
         }
+
+        
+        private static RSA rsa = new RSA();
+        private static SymetricEncryptionByte des = new SymetricEncryptionByte();
+        private static HashFunction hf = new HashFunction();
+
         static void Main(string[] args)
         {
 
-            var p = WMI.GetProcessorInfo();
-            var b = WMI.GetBaseBoardInfo();
+            //Create test text file 
+            string plainTextPath = "plainText.txt";
+            string message = "test message";
+            File.WriteAllText(plainTextPath, message);
+
+            //Console.WriteLine(Encoding.Default.GetString(M)); // з байтів в строку 
+
+            //H(M)
+            #region H(M)
+
+            //4 ulongs for HashFunction
+            ulong[] iv = new ulong[4]; for (int i = 0; i < iv.Length; i++) iv[i] = PrimeNumberGenerator.Generate();
+            string hashTextPath = "hasText.txt";
+
+            byte[] hashM = hf.CreateHashCode(plainTextPath, hashTextPath, iv);
+
+            #endregion
+
+            //A Keys
+            #region A Keys
+
+            BigInteger PublicKeyA;
+            BigInteger PrivateKeyA;
+            BigInteger n_a = rsa.PublicKey_PrivateKey(43, 59, out PublicKeyA, out PrivateKeyA); //43, 59 => PrimeNumberGenerator.Generate();
+
+            #endregion
             
+            //RSA_A(H(M))
+            #region RSA_A(H(M))
 
-            Console.WriteLine(p.GetHashCode());
-            Console.WriteLine(b.GetHashCode());
+            string rsaAHashPath = "rsaAHash.txt";
+            rsa.EncryptFileRSA(hashTextPath, rsaAHashPath, PublicKeyA, n_a);
 
+            #endregion
 
+            //M || RSA_A(H(M))
+            #region M || RSA_A(H(M))
 
+            byte[] M = File.ReadAllBytes(plainTextPath);
+            byte[] rsaHash = File.ReadAllBytes(rsaAHashPath);
+            byte[] rsaHashPlusMessage = rsaHash.Concat(M).ToArray();
 
+            string rsaHashPlusMessagePath = "rsaHashPlusMessage.txt";
+            File.WriteAllBytes(rsaHashPlusMessagePath, rsaHashPlusMessage);
+            
+            #endregion
+
+            //DES(M || RSA_A(H(M))), Session key
+            #region DES(M || RSA_A(H(M))), Skey
+
+            string desPath = "des.txt";
+            int addByte = 0;
+
+            ulong k = PrimeNumberGenerator.Generate();
+            byte[] sesKey = new byte[8];
+            byte[] array = des.UlongToByte((long)k);
+            Array.Copy(array, 0, sesKey, 0, 4);
+            k = PrimeNumberGenerator.Generate();
+            array = des.UlongToByte((long)k);
+            Array.Copy(array, 0, sesKey, 4, 4);
+
+            byte[] decodeKey = des.EncryptFile(rsaHashPlusMessagePath, desPath, sesKey, out addByte);
+            string sesKeyPath = "sesKey.txt";
+            File.WriteAllBytes(sesKeyPath, sesKey);
+
+            #endregion
+
+            //B Keys
+            #region B Keys
+
+            BigInteger PublicKeyB;
+            BigInteger PrivateKeyB;
+            BigInteger n_b = rsa.PublicKey_PrivateKey(43, 59, out PublicKeyB, out PrivateKeyB); //43, 59 => PrimeNumberGenerator.Generate();
+
+            #endregion
+
+            //RSA_B(sesKey)
+            #region RSA_B(sesKey)
+
+            string rsaSesKeyPath = "rsaBSes.txt";
+            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! n_a or n_b, PublicB or PrivateB
+            rsa.EncryptFileRSA(sesKeyPath, rsaSesKeyPath, PublicKeyB, n_b);
+
+            #endregion
+
+            //DES(M || RSA_A(H(M))) || RSA_B(sesKey)
+            #region DES(M || RSA_A(H(M))) || RSA_B(sesKey)
+
+            string finalPath = "final.txt";
+
+            byte[] d = File.ReadAllBytes(desPath);
+            byte[] rb = File.ReadAllBytes(rsaSesKeyPath);
+            byte[] final = d.Concat(rb).ToArray();
+            File.WriteAllBytes(finalPath, final);
+
+            //Console.WriteLine($"d: {d.Length}\nrb: {rb.Length}\nfinal: {final.Length}");
+
+            #endregion
 
             Console.WriteLine("End");
             Console.ReadLine();
